836d49f2c4d2a3e6af2ba08b666df2d0ee091fad
Jacobo24 <jaccalgar@gmail.com>
2022-12-02 23:38:45 +0100

    laberinto

--- laberinto.py
+++ laberinto.py
@@ -10,3 +10,8 @@ def laberinto(tamaño, limite):
         laberinto.append(fila)
     return laberinto
 
+limite = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
+lab = laberinto(5, limite)
+
+for i in lab:
+    print(''.join(i))
