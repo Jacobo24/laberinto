e9487134054b585d0613c2ab73fe8e6aaf3ccc97
Jacobo24 <jaccalgar@gmail.com>
2022-12-08 22:23:01 +0100

    fin?

--- laberinto.py
+++ laberinto.py
@@ -15,3 +15,32 @@ lab = laberinto(5, limite)
 
 for i in lab:
     print(''.join(i))
+
+# Path: laberinto.py
+def laberinto(abajo, derecha, arriba, izquierda):
+    while True:
+        if abajo and abajo :
+            print("continua")
+        else :
+            print("perdiste")
+            break
+        if derecha and derecha:
+            print("continua")
+        else :
+            print("perdiste")
+            break
+        if arriba and arriba:
+            print("continua")
+        else :
+            print("perdiste")
+            break
+        if derecha and derecha:
+            print("continua")
+        else :
+            print("perdiste")
+            break
+        if abajo and abajo:
+            print("ganaste")
+        else :
+            print("perdiste")
+            break
